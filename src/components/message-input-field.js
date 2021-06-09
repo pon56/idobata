c4eb3f2bbbca4411f13bcb099cbf9bea69c8f63e
pon56 <sirana9993@gmail.com>
2021-06-09 19:31:50 +0900

    apply grid input field

--- src/components/message-input-field.js
+++ src/components/message-input-field.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Avatar } from '@material-ui/core';
+
+const useStyles =  makeStyles({
+    root:{
+        gridRow: 2,
+        margin: '26px',
+    }
+})
 
 const MessageListInputField = () =>{
+    const Classes = useStyles();
     return(
-        <div>
-         MessageListInputField
+        <div className = {Classes.root}>
+        <Grid container>
+            <Grid xs={1}>
+                <Avatar />
+            </Grid>
+            <Grid xs={10}>
+                入力
+            </Grid>
+            <Grid xs={1}>
+                ボタン
+            </Grid>
+        </Grid>
         </div>
     )
 }
